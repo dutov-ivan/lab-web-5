b9015dae84e68adf0f84ea0715abbe421ee976af
Ivan Dutov <ivann.dutov@gmail.com>
2025-11-10 23:00:21 +0200

    task 2

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const xHeading = document.querySelector("#company-heading");
 const yHeading = document.querySelector("#explore-more");
+const block5 = document.querySelector("#main-content");
 
 let temp;
 temp = xHeading.innerHTML;
 xHeading.innerHTML = yHeading.innerHTML;
 yHeading.innerHTML = temp;
+
+const height = 100;
+const width = 200;
+
+function displayRectangleArea(height, width) {
+  const area = height * width;
+  const el = document.createElement("p");
+  el.textContent = area.toString();
+  block5.appendChild(el);
+}
+
+displayRectangleArea(height, width);
